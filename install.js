66d3284d510e2bc976c94fc30ab942116d30e321
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2024-11-22 17:17:08 +0100

    huggingface_hub fix

--- install.js
+++ install.js
@@ -30,8 +30,7 @@ module.exports = {
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "pip install gradio devicetorch",
-          "pip install -r requirements.txt",
-          "pip install --upgrade huggingface-hub"
+          "pip install -r requirements.txt"
         ]
       }
     },
