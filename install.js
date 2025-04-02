6aa118292eba4eaa71028ed1461873779a62324f
6Morpheus6 <wanton-decay@gmx.net>
2025-04-03 01:51:00 +0200

    pin pydantic

--- install.js
+++ install.js
@@ -29,7 +29,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install gradio==5.6.0 devicetorch",
+          "uv pip install gradio==5.6.0 pydantic==2.10.6 devicetorch",
           "uv pip install -r requirements.txt"
         ]
       }
